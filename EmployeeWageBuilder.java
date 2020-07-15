602004bc71f7aa856c12a22b4608578bc1321537
mukesh <mukeshmahajan501@gmail.com>
2020-07-15 15:38:48 +0530

    added daily employee wage code in Uc2

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,19 +1,19 @@
-class EmployeeWageBuilder
-{
+public class EmployeeWageBuilder{
 
-public static void main(String args[])
-{
+public static void main(String args[]){
+        int IS_FULL_TIME=1;
+        int EMP_RATE_PER_HOUR=20;
 
-int isFullTime=1;
-double checkEmp=Math.floor(Math.random()*10)%2;
-        if (checkEmp == isFullTime)
-        {
-                System.out.println("employee is present");
-        }
-        else
-        {
-                System.out.println("employee is absent");
-        }
+        int empHrs=0;
+        int empWage=0;
+        double empCheck=Math.floor(Math.random() *10 )% 2;
+if (empCheck == IS_FULL_TIME)
+        empHrs=8;
+else
+        empHrs=0;
+
+empWage=empHrs*EMP_RATE_PER_HOUR;
+System.out.println("Daily Employee Wage is : "+empWage);
 
 }
 }
